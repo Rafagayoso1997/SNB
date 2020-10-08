d38f9a79dc2d73d5730035763db37f5a44b88b45
Rafael <gayoso0597@gmail.com>
2020-10-08 21:40:59 +0200

    Added Teams Itinerary View

--- TransporteSerieNacionalFX/src/controller/TeamsItineraryController.java
+++ TransporteSerieNacionalFX/src/controller/TeamsItineraryController.java
@@ -30,15 +30,17 @@ public class TeamsItineraryController implements Initializable {
     private  Controller controller;
     public static int selectedCalendar;
     @FXML
-    private JFXListView<String> teamsListView;
+    private JFXListView teamsListView;
 
     @FXML
-    private TableView<TeamItineraryName> itineraryTable;
+    private TableView<ObservableList> itineraryTable;
 
     @FXML
     private JFXButton showItinerary;
 
     private HomeController homeController;
+
+    private ObservableList<ObservableList> data;
 
 
 
@@ -60,6 +62,7 @@ public class TeamsItineraryController implements Initializable {
 
     @FXML
     void displayItinerary(ActionEvent event) {
+        data = FXCollections.observableArrayList();
 
         itineraryTable.getColumns().removeAll(itineraryTable.getColumns());
         itineraryTable.setItems(FXCollections.observableArrayList(new ArrayList<>()));
@@ -70,19 +73,45 @@ public class TeamsItineraryController implements Initializable {
         ArrayList<ArrayList<Integer>> itinerary = controller.teamsItinerary(calendar,configuration);
 
 
+        /*
+        Poner el encabezado de las columnas y el valor que va dentro 
+         */
+        int index=0;
         for (int selectedTeam : selectedTeams) {
-            TableColumn col = new TableColumn<>(controller.getAcronyms().get(selectedTeam));
-            col.setCellValueFactory(new PropertyValueFactory<>("team"));
+            final int j= index;
+            TableColumn col = new TableColumn(controller.getAcronyms().get(selectedTeam));
+            col.setCellValueFactory((Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>)
+                    param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+            index++;
             itineraryTable.getColumns().add(col);
         }
 
-        for (int selectedTeam : selectedTeams) {
-            ArrayList<Integer> currentItinerary = itinerary.get(selectedTeam);
+
+        for(int i=0; i < itinerary.size();i++){
+            ObservableList<String> row = FXCollections.observableArrayList();
+            ArrayList<Integer> current = itinerary.get(i);
+            for (int selectedTeam : selectedTeams) {
+                row.add(controller.getAcronyms().get(current.get(selectedTeam)));
+            }
+            data.add(row);
+        }
+
+        itineraryTable.setItems(data);
+
+        /*for (int selectedTeam : selectedTeams) {
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                //Iterate Column
+                row.add(rs.getString(i));
+            }
+            System.out.println("Row [1] added "+row );
+            data.add(row);
+           /* ArrayList<Integer> currentItinerary = itinerary.get(selectedTeam);
             for(int teamIndex: currentItinerary){
                 String team = controller.getAcronyms().get(teamIndex);
                 itineraryTable.getItems().add(new TeamItineraryName(team));
-            }
-        }
+            }*/
+        //}
         
 
     }
